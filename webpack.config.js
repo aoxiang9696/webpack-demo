7921e2965ef4bc9605048f41056c6ad6c73ad0ae
aoxiang9696 <aoxiang9696@163.com>
2022-01-06 09:46:43 +0800

    fix: 修复webpack5+devServer引发的兼容性问题

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,28 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    // devServer: { //(如果不注释掉 npm run serve起不来，原因待查)
-    //     contentBase: path.resolve(__dirname, 'dist') // 开发服务器启动路径
-    // },
-    module: {
-        rules: [
-            {
-                text: /\.jsx?/, // 匹配文件类型
-                include: [
-                    path.resolve(__dirname, 'src') //指定哪些路径下文件需要loader处理
-                ],
-                use: {//指定使用的loader
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+    devServer: { //(如果不注释掉 npm run serve起不来，原因待查)
+        // contentBase: path.join(__dirname, 'dist') // 开发服务器启动路径
+        static: {
+            directory: path.join(__dirname, 'dist'),
+          },
+    },
+    // module: {
+    //     rules: [
+    //         {
+    //             text: /\.jsx?/, // 匹配文件类型
+    //             include: [
+    //                 path.resolve(__dirname, 'src') //指定哪些路径下文件需要loader处理
+    //             ],
+    //             use: {//指定使用的loader
+    //                 loader: 'babel-loader',
+    //                 options: {
+    //                     presets: ['@babel/preset-env']
+    //                 }
+    //             }
+    //         }
+    //     ]
+    // }
 
 
 }
